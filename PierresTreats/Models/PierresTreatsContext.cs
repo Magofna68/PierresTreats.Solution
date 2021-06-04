afb77c73f9eb500a0dd4478bcac80035fa4a8ed5
Brandon-Magofna <magofna68@gmail.com>
2021-06-04 08:51:04 -0700

    :sparkles: build out all Models

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavor {get; set;}
+    public DbSet<Treat> Treat {get; set;}
+    public DbSet<FlavorTreat> FlavorTreat {get; set;}
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+
+  }
+}
