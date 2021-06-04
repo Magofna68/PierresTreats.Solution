0a168226da1b55b58ca77ca665b9c94930386e95
Brandon-Magofna <magofna68@gmail.com>
2021-06-04 09:28:35 -0700

    :sparkles: build out Controllers/Home

---
+++ PierresTreats/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierresTreats.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Treat> treat = _db.Treat.ToList();
+      List<Flavor> flavor = _db.Flavor.ToList();
+      model.Add("treat", treat);
+      model.Add("flavor", flavor);
+      return View(model);
+    }
+  }
+}
