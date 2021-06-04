9215dbe7aa13b4c9e7544878b6c97b45a73ec286
Brandon-Magofna <magofna68@gmail.com>
2021-06-04 10:01:35 -0700

    :sparkles: build out Controllers/Treat

---
+++ PierresTreats/Controllers/TreatController.cs
@@ -0,0 +1,130 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers
+{
+  public class TreatController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatController(UserManager<ApplicationUser> userManager, PierresTreats db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindOrDefault(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userTreat = _db.Treat.Where(entry=>entry.User.Id == currentUser.Id).ToList();
+      return View(userTreat);
+    }
+[   Authorize]
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavor, "FlavorId", "Name", "Quantity", "Calories", "Rating");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treat.Add(treat);
+      _db.SaveChanges();
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() {FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treat
+        .Include(Treat=>Treat.JoinEntities)
+        .ThenInclude(join=>join.Flavor)
+        .FirstOrDefault(treat=>treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisTreat = _db.Treat.FirstOrDefault(treat=>treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavor, "FlavorId", "Type");
+      return View(thisTreat);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult Edit(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat()
+        { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treat.FirstOrDefault(treat=>treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavor, "FlavorId", "Type");
+      return View(thisTreat);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treat.FirstOrDefault(treat=>treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [Authorize]
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treat.FirstOrDefault(treat=>treat.TreatId == id);
+      _db.Treat.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntity = _db.FlavorTreat.FirstOrDefault(entry=>entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
